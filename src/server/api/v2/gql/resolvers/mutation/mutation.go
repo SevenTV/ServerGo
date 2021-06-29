725caab8b8ce64301b61bbebf644b399cb72082d
Anatole <me@anatole.dev>
2021-06-29 14:42:01 +0200

    API/GQL - User Mutation

--- src/server/api/v2/gql/resolvers/mutation/mutation.go
+++ src/server/api/v2/gql/resolvers/mutation/mutation.go
@@ -14,3 +14,8 @@ type emoteInput struct {
 	Visibility *int32    `json:"visibility"`
 	Tags       *[]string `json:"tags"`
 }
+
+type userInput struct {
+	ID     string  `json:"id"`
+	RoleID *string `json:"role_id"`
+}
