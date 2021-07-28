1b8b2d30e4e2d8f3c4a8ea1f0bcf702dfdfbbeec
Anatole <me@anatole.dev>
2021-07-28 06:31:43 +0200

    Check permission for adding zerowidth to channel

--- src/mongo/datastructure/datastructure.go
+++ src/mongo/datastructure/datastructure.go
@@ -213,6 +213,7 @@ const (
 	RolePermissionEditEmoteGlobalState                   // 1024 - (Elevated) Allows editing the global state of an emote
 	RolePermissionEditApplicationMeta                    // 2048 - (Elevated) Allows editing global app metadata, such as the active featured broadcast
 	RolePermissionManageEntitlements                     // 4096 - (Elevated) Allows granting and revoking entitlements to and from users
+	RolePermissionUseZerowidthEmote                      // 8192 - Allows zero-width emotes to be enabled
 
 	RolePermissionAll int64 = (1 << iota) - 1
 )
